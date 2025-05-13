6290cbf08ce0315c4dd73238f59941bd2265e6d0
HectorAlejandro26 <alex2611alejandro@gmail.com>
2025-05-12 21:07:22 -0700

    Añadir soporte para la construcción de HillCipher
    Se ha añadido la carpeta `bin_py` a `.gitignore`.
    Se han actualizado `HillCipher.spec` para definir el análisis y la creación del ejecutable con PyInstaller.
    Se ha incluido `HillCipher.exe` en el archivo `ProyectoMetodos.csproj` para su copia en el directorio de salida.
    Se ha eliminado la tarea `PostBuild` que gestionaba la copia del ejecutable.
    Se han añadido comprobaciones en `build_py.ps1` para crear el directorio `bin_py` y activar el entorno virtual de Python antes de ejecutar PyInstaller.

---
+++ build_py.ps1
@@ -0,0 +1,9 @@
+if (-not Test-Path "./bin_py") {
+    New-Item -Type Directory ./bin_py
+}
+
+if (-not $env:VIRTUAL_ENV) {
+    & ./.venv/Scripts/Activate.ps1;
+}
+
+pyinstaller --onefile --console --name "HillCipher" --distpath .\bin_py --workpath "pyinstaller_temp" main.py;
